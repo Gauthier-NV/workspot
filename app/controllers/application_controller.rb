bc6f6bb250aacd79a3ff9e03f253ab834f271e1b
Gauthier NOUVEAU <gauthier.nouveau@gmail.com>
2025-06-30 14:05:26 +0200

    mailer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+# Ensure all requests are redirected to the www subdomain
+# This is useful for SEO and consistency
+# If the request host is not www, redirect to the www version of the URL
+# Example: workspots.fr -> www.workspots.fr
+
 end
