4cc3870a42bb8992aa139e818061ce53370fff74
Gauthier NOUVEAU <gauthier.nouveau@gmail.com>
2025-09-05 17:54:12 +0200

    mise à jour Form + effet navbar sur smartophone

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -3,45 +3,40 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    this.shrunk = false;
+    this.shrunk = false
 
-    // shrink au scroll
+    // shrink uniquement sur desktop
     if (window.innerWidth > 768) {
-      window.addEventListener("scroll", this.handleScroll);
+      window.addEventListener("scroll", this.handleScroll)
     }
 
-    // hide sur la bannière "about"
-    const about = document.querySelector(".banner.about-banner");
+    // hide sur la bannière About Us (mobile + desktop)
+    const about = document.querySelector(".banner.about-banner")
     if (about) {
       this.observer = new IntersectionObserver(([entry]) => {
         if (entry.isIntersecting) {
-          this.element.classList.add("is-hidden");
+          this.element.classList.add("is-hidden")
         } else {
-          this.element.classList.remove("is-hidden");
+          this.element.classList.remove("is-hidden")
         }
-      }, { threshold: 0.7 });
-
-      this.observer.observe(about);
+      }, { threshold: 0.7 }) // 0.5 = moitié visible → navbar cachée
+      this.observer.observe(about)
     }
   }
 
   disconnect() {
-    window.removeEventListener("scroll", this.handleScroll);
-
-    if (this.observer) {
-      this.observer.disconnect();
-    }
+    window.removeEventListener("scroll", this.handleScroll)
+    if (this.observer) this.observer.disconnect()
   }
 
   handleScroll = () => {
-    const shouldShrink = window.scrollY > 3;
-
+    const shouldShrink = window.scrollY > 3
     if (shouldShrink && !this.shrunk) {
-      this.element.classList.add("shrink");
-      this.shrunk = true;
+      this.element.classList.add("shrink")
+      this.shrunk = true
     } else if (!shouldShrink && this.shrunk) {
-      this.element.classList.remove("shrink");
-      this.shrunk = false;
+      this.element.classList.remove("shrink")
+      this.shrunk = false
     }
   }
 }
