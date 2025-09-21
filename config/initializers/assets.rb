fbcbc9af9da30a93b099bc755c424da8431f0117
Gauthier NOUVEAU <gauthier.nouveau@gmail.com>
2025-09-21 22:20:34 +0200

    working map debug

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,12 +1,9 @@
 # Be sure to restart your server when you modify this file.
 
-# Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = "1.0"
 
-# Add additional assets to the asset load path.
-# Rails.application.config.assets.paths << Emoji.images_path
+# Dites à Sprockets où trouver les sources JS gérées par Importmap
+Rails.application.config.assets.paths << Rails.root.join("app/javascript")
+#Rails.application.config.assets.paths << Rails.root.join("vendor/javascript") # si tu l'utilises
 
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in the app/assets
-# folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+# Ne pas ajouter application.js à assets.precompile ici.
