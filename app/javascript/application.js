0b2696f20ea126be179d988ec1acd6a8fe1ec68a
Gauthier NOUVEAU <gauthier.nouveau@gmail.com>
2025-10-06 11:54:59 +0200

    mise à jour 1 application.js + navabar buton plus visible

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,15 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+// app/javascript/application.js
+
+// Import Turbo et Stimulus (via importmap)
 import "@hotwired/turbo-rails"
+import { Application } from "@hotwired/stimulus"
 import "controllers"
+
+// Lance l'application Stimulus
+const application = Application.start()
+
+// Configure Stimulus pour le dev
+application.debug = false
+window.Stimulus = application
+
+export { application }
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,4 +1,8 @@
-// Import and register all your controllers from the importmap via controllers/**/*_controller
-import { application } from "controllers/application"
+import { Application } from "@hotwired/stimulus"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+
+const application = Application.start()
+application.debug = false
+window.Stimulus = application
+
 eagerLoadControllersFrom("controllers", application)
