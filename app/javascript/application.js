cc7a9c67554573862c434e3dda68797150ba09a1
Gauthier NOUVEAU <gauthier.nouveau@gmail.com>
2025-10-07 13:24:17 +0200

    maj map

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,15 +1,2 @@
 // app/javascript/application.js
-
-// Import Turbo et Stimulus (via importmap)
-import "@hotwired/turbo-rails"
-import { Application } from "@hotwired/stimulus"
-import "controllers"
-
-// Lance l'application Stimulus
-const application = Application.start()
-
-// Configure Stimulus pour le dev
-application.debug = false
-window.Stimulus = application
-
-export { application }
+import "controllers"; // Stimulus est démarré dans controllers/index.js
